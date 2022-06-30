049e2f1af497ddd971aa138fa56cdc5536c249e4
MustafaPaigeer <mustafapaigeer@live.com>
2022-06-30 00:11:30 -0700

    create jwt token

--- queries.js
+++ queries.js
@@ -32,13 +32,13 @@ const getTasks = (req, res) => {
   pool
     .query('SELECT * FROM tasks ORDER BY id DESC')
     .then(data => res.status(200).json(data.rows))
-    .catch(err => res.status(500).json('Error executing query', err.message))
+    .catch(err => res.status(500).json({message: 'Error executing query'}))
 };
 
 const createTask = (req, res) => {
-  console.log(req.body)
+  //console.log(req.body)
   const { user_id, category, description, status } = req.body;
-  console.log(user_id)
+  //console.log(user_id)
   pool
     .query(`Insert Into tasks (user_id, category, description, status) Values($1,$2,$3,$4) RETURNING *`, [user_id, category, description, status])
     .then(data => res.send(`Todo item added successfully`))
--- routes/auth.js
+++ routes/auth.js
@@ -12,10 +12,10 @@ router.post('/login', async (req, res) => {
     const {email, password} = req.body;
     const users = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
     //Check if the email exists
-    if(users.rows.length === 0) return res.status(401).json({error: "Email or Password is incorrect"});
+    if(users.rows.length === 0) throw error;//return res.status(401).json({error: "Email or Password is incorrect"});
     //Check the Password
     const validPassword = await bcrypt.compare(password, users.rows[0].password);
-    if(!validPassword) return res.status(401).json({error: "Incorrect Email or Password"});
+    if(!validPassword) throw error;//return res.status(401).json({error: "Incorrect Email or Password"});
     // CREATE AND SEND JWt TOKEN
     return res.status(200).json("success")
   } catch (error) {
